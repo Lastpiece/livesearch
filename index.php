2f2994bd3a842291a09d33d30d62626f1bb76d0f
LAKHDARI Hamza <h.lakhdari@talis-bs.net>
2021-05-06 13:50:40 +0200

    first

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Livesearch</title>
+</head>
+<body>
+    <h1>Live Search de l'index des élèves de la promo 2021 Dév FullStack</h1>
+    <form action="search.php" method="POST">
+    <input type="text" name="name" id="name" placeholder="Search by name here">
+    </form>
+    <ul id="data-list">
+        <li id="data-item1">Eleve 1</li>
+        <li id="data-item2">Eleve 2</li>
+        <li id="data-item3">Eleve 3</li>
+    </ul>
+</body>
+</html>
